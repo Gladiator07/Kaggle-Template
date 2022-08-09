5211519c611974efdfd835cf4cd05f39e3358844
Gladiator07 <atharvaaingle@gmail.com>
2022-08-09 23:08:20 +0530

    upd utils

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,24 @@
-import slack
+import os
+import random
 from types import SimpleNamespace
+
+import numpy as np
+import slack
+import torch
+
+
+def set_seed(
+    seed: int = 42, deterministic: bool = False, benchmark: bool = False
+) -> None:
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = deterministic
+    torch.backends.cudnn.benchmark = benchmark
+    torch.use_deterministic_algorithms(deterministic, warn_only=True)
+    print(f"Global seed set to {seed}")
 
 
 class SlackNotifier:
