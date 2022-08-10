fd5dcc012eee02bc74700866bb15db05005be6bc
Gladiator07 <atharvaaingle@gmail.com>
2022-08-10 11:25:21 +0530

    fix

--- configs/default_config.py
+++ configs/default_config.py
@@ -7,31 +7,36 @@ from types import SimpleNamespace
 PROJECT = ""
 
 
-def determine_env_and_set_path(cfg: SimpleNamespace) -> SimpleNamespace:
+def determine_env_and_set_root_paths(cfg: SimpleNamespace) -> SimpleNamespace:
     """
     Determines current environment and set root directory paths
     """
     project = cfg.project
+
     if "google.colab" in sys.modules:
         cfg.environ = "colab"
         cfg.code_dir = Path(f"/content/{project}")
         cfg.artifacts_dir = Path(f"/content/artifacts")
+
     elif os.environ.get("KAGGLE_KERNEL_RUN_TYPE", ""):
         cfg.environ = "kaggle"
         cfg.code_dir = Path(f"/kaggle/working/{project}")
         cfg.artifacts_dir = Path(f"/kaggle/working/artifacts")
+
     elif pkgutil.find_loader("jarviscloud"):
         cfg.environ = "jarvislabs"
-        cfg.code_dir = Path(f"/home{project}")
+        cfg.code_dir = Path(f"/home/{project}")
         cfg.artifacts_dir = Path(f"/home/artifacts")
+
     else:
         cfg.environ = "local"
+
     return cfg
 
 
 # default config
 cfg = SimpleNamespace(**{})
 cfg.project = PROJECT
-cfg = determine_env_and_set_path(cfg)
+cfg = determine_env_and_set_root_paths(cfg)
 
 basic_config = cfg
