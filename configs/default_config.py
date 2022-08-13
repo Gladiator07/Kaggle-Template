c0fbe8d80b6689ac28c4b9f0b02a04dba3798497
Gladiator07 <atharvaaingle@gmail.com>
2022-08-13 11:57:09 +0530

    upd default config

--- configs/default_config.py
+++ configs/default_config.py
@@ -2,12 +2,13 @@ import os
 import pkgutil
 import sys
 from pathlib import Path
-from types import SimpleNamespace
+
+import ml_collections as mlc
 
 PROJECT = ""
 
 
-def determine_env_and_set_root_paths(cfg: SimpleNamespace) -> SimpleNamespace:
+def determine_env_and_set_root_paths(cfg: mlc.ConfigDict) -> mlc.ConfigDict:
     """
     Determines current environment and set root directory paths
     """
@@ -16,27 +17,32 @@ def determine_env_and_set_root_paths(cfg: SimpleNamespace) -> SimpleNamespace:
     if "google.colab" in sys.modules:
         cfg.environ = "colab"
         cfg.code_dir = Path(f"/content/{project}")
-        cfg.artifacts_dir = Path(f"/content/artifacts")
+        cfg.artifacts_dir = Path("/content/artifacts")
 
     elif os.environ.get("KAGGLE_KERNEL_RUN_TYPE", ""):
         cfg.environ = "kaggle"
         cfg.code_dir = Path(f"/kaggle/working/{project}")
-        cfg.artifacts_dir = Path(f"/kaggle/working/artifacts")
+        cfg.artifacts_dir = Path("/kaggle/working/artifacts")
 
     elif pkgutil.find_loader("jarviscloud"):
         cfg.environ = "jarvislabs"
         cfg.code_dir = Path(f"/home/{project}")
-        cfg.artifacts_dir = Path(f"/home/artifacts")
+        cfg.artifacts_dir = Path("/home/artifacts")
 
     else:
+        # raise NotImplementedError("Current Environment is not recognized")
         cfg.environ = "local"
-
     return cfg
 
 
 # default config
-cfg = SimpleNamespace(**{})
+cfg = mlc.ConfigDict()
 cfg.project = PROJECT
 cfg = determine_env_and_set_root_paths(cfg)
 
-basic_config = cfg
+# add default config specific to project here
+# ...
+
+
+def get_default_config():
+    return cfg
