d8968f0ce4bbe046b103d259d91c62f26a731bfa
Gladiator07 <atharvaaingle@gmail.com>
2022-08-14 10:00:53 +0530

    fix path

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd src/
-python train.py --config ../configs/cfg_baseline.py \
+python train.py --config configs/cfg_baseline.py \
                 --fold 0 \
                 --config.version 0 \
                 --config.notes "baseline experiment"
