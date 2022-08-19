f51b07123a712873e44bb42ebd10ecd13148d158
Gladiator07 <atharvaaingle@gmail.com>
2022-08-20 00:04:21 +0530

    upd boiler place cfg and train for new custom trainer

--- configs/cfg_baseline.py
+++ configs/cfg_baseline.py
@@ -4,26 +4,26 @@ from default_config import get_default_config
 cfg = get_default_config()
 cfg.name = os.path.basename(__file__).split(".")[0]
 cfg.seed = 7
-
+cfg.deterministic = False
+cfg.benchmark = False
 # data
-cfg.datamodule = dict()
+
 
 # model
 cfg.model = dict()
 
 # trainer
-cfg.trainer = dict(
-    accelerator="gpu",
-    accumulate_grad_batches=1,
-    auto_lr_find=False,
-    benchmark=False,
-    deterministic=False,  # "warn"
-    fast_dev_run=False,
-    gradient_clip_val=1.0,
-    overfit_batches=0.0,
-    precision=16,
-    max_epochs=5,
-    val_check_interval=1.0,  # float in range [0.0, 1.0] to check after fraction of epoch
+cfg.trainer_args = dict(
+    num_train_epochs=5,
+    gradient_accumulation_steps=1,
+    max_grad_norm=1.0,
+    mixed_precision="fp16",  # bf16, no
+    scheduler_type="cosine",  # linear
+    num_warmup_steps=0.1,  # exact steps (int) can be also passed
+    save_best_checkpoint=True,
+    save_last_checkpoint=True,
+    save_weights_only=True,
+    metric_for_best_model="val/accuracy",
 )
 
 # tracking
@@ -32,6 +32,8 @@ cfg.version = 0
 cfg.experiment_name = f"{cfg.name}_v{cfg.version}"
 # override from cmd for short overview of the experiment
 cfg.notes = "baseline experiment"
+
+cfg.wandb_args = dict(group=cfg.experiment_name, notes=cfg.notes, save_code=True)
 
 
 def get_config():
--- src/train.py
+++ src/train.py
@@ -1,6 +1,8 @@
 import sys
-from absl import app, flags
+from absl import flags
 from ml_collections import config_flags
+from accelerate import Accelerator
+from utils import set_seed
 
 sys.path.append("../configs")
 
@@ -10,22 +12,39 @@ config_flags.DEFINE_config_file(
     help_string="Training Configuration from `configs` directory",
 )
 flags.DEFINE_integer("fold", default=0, help="fold index")
-flags.DEFINE_bool(
-    "debug", default=False, help="debug pipeline with logging and tracking disabled"
-)
+flags.DEFINE_bool("debug", default=False, help="debug pipeline with logging and tracking disabled")
 flags.DEFINE_bool("wandb_enabled", default=True, help="enable Weights & Biases logging")
 
 FLAGS = flags.FLAGS
+FLAGS(sys.argv)  # need to explicitly to tell flags library to parse argv before you can access FLAGS.xxx
 cfg = FLAGS.config
 debug = FLAGS.debug
 fold = FLAGS.fold
 wandb_enabled = FLAGS.wandb_enabled
 
 
-def main(argv):
-    del argv
+def main():
     # train script here
+    set_seed(cfg.seed, cfg.deterministic, cfg.benchmark)
+
+    # init 🤗 Accerelator
+    accelerator = Accelerator(
+        device_placement=True,
+        step_scheduler_with_optimizer=False,
+        mixed_precision=cfg.trainer_args.mixed_precision,
+        gradient_accumulation_steps=cfg.trainer_args.gradient_accumulation_steps,
+        log_with="wandb" if wandb_enabled else None,
+    )
+
+    if wandb_enabled:
+        accelerator.init_trackers(project_name=cfg.project, config=cfg.to_dict(), init_kwargs=cfg.wandb_args)
+
+    # rest of the script ....
+
+    # end tracker
+    if wandb_enabled:
+        accelerator.end_training()
 
 
 if __name__ == "__main__":
-    app.run(main)
+    main()
